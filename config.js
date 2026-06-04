849cbecfabcd17d4b063474e1843a467fb176d80
ahmedabdelmn3m <ahmed.m.abdelmn3m@gmail.com>
2026-06-04 11:47:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 //  Replace API_URL with your Railway domain
 // ─────────────────────────────────────────────
 const CONFIG = {
-  API_URL: 'https://YOUR-RAILWAY-URL.up.railway.app'
+  API_URL: 'https://wospredv3-production.up.railway.app'
 };
