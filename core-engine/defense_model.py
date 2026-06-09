63cb0fe62f17dbe3c569038997c34269d84f845f
ahmedabdelmn3m <ahmed.m.abdelmn3m@gmail.com>
2026-06-09 17:12:46 +0300

    Delete core-engine directory

--- core-engine/defense_model.py
+++
@@ -1,7 +0,0 @@
-class DefenseModel:
-    @staticmethod
-    def calculate_defense_power(defense, defense_stats_sum, health, health_stats_sum):
-        """
-        DefensePower = 100 * Defense * (1 + ΣDefenseStats) * Health * (1 + ΣHealthStats)
-        """
-        return 100 * defense * (1 + defense_stats_sum) * health * (1 + health_stats_sum)
